0116a65ba63bde02d19cb7562821673c8521b992
alexxxchange <80538535+alexxxchange@users.noreply.github.com>
2023-04-25 16:19:00 -0400

    audio manager with capacity for lots of mobs. first draft on a couple sounds

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,147 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+
+    [Header("Sources")]
+    [SerializeField] AudioSource[] playerSfxSources;
+    [SerializeField] AudioSource[] mobSfxSources;
+    [SerializeField] AudioSource uiAudioSource;
+    [SerializeField] AudioSource[] bgmSources;
+    [SerializeField] Transform playerSfxSourceParent;
+    [SerializeField] Transform mobSfxSourceParent;
+
+    [Header("Clips")]
+    [SerializeField] List<AudioClip> clips;
+    [SerializeField] MultiSound[] multiSounds;
+
+
+    [Header("Audio Mixer and Snapshots")]
+    [SerializeField] AudioMixer mainMixer;
+    [SerializeField] AudioMixer mobChatterMixer;
+    [SerializeField] AudioMixerSnapshot overWorldSnapshot;
+    [SerializeField] AudioMixerSnapshot battleSnapshot;
+    [SerializeField] AudioMixerSnapshot lightMobs;
+    [SerializeField] AudioMixerSnapshot medMobs;
+    [SerializeField] AudioMixerSnapshot heavyMobs;
+
+    int currentMobSource = 0;
+    int currentPlayerSource = 0;
+
+    public Dictionary<string, AudioClip> audioDict = new Dictionary<string, AudioClip>();
+    public Dictionary<string, MultiSound> multiSoundDict = new Dictionary<string, MultiSound>();
+
+
+
+    void Awake()
+    {
+
+        if (instance != null && instance != this)
+            Destroy(this.gameObject);
+        else
+        {
+            instance = this;
+        
+        }
+        foreach (AudioClip clip in clips)
+        {
+            audioDict.Add(clip.name, clip);
+        }
+        foreach (MultiSound multi in multiSounds)
+        {
+            multiSoundDict.Add(multi.name, multi);
+        }
+
+
+        playerSfxSources = playerSfxSourceParent.GetComponentsInChildren<AudioSource>();
+        mobSfxSources = mobSfxSourceParent.GetComponentsInChildren<AudioSource> ();
+
+        StartBGM();
+    }
+
+    private void StartBGM()
+    {
+        //set up mixer snapshots and play both loops
+    }
+
+    public void PlayOverworldMusic()
+    {
+       
+    }
+
+    public void PlayBattleMusic()
+    {
+        // mixer snapshot fade
+    }
+
+    public void PlaySingleClip(string clipName, SFXCategory category, float pitchVariance, float volumeVariance)
+    {
+        if (audioDict.ContainsKey(clipName)) RouteClipToSourcesAndPlay(audioDict[clipName], category, pitchVariance, volumeVariance);
+
+        else Debug.Log("AudioManager: AudioClip " + clipName + " not found in dictionary.");
+    }
+
+    public void PlayShuffledSound(string soundName, SFXCategory category, float pitchVariance, float volumeVariance)
+    {
+        print("called multisound from audio manager");
+        if (multiSoundDict.ContainsKey(soundName))
+        {
+            print("foudn the multi sound");
+            var tempClip = multiSoundDict[soundName].GetShuffledClip();
+            if (tempClip != null) RouteClipToSourcesAndPlay(tempClip, category, pitchVariance, volumeVariance);
+        }
+        else
+        {
+            Debug.Log("AudioManager: AudioClip " + soundName + " not found in multi sound dictionary.");
+        }
+    }
+
+    void RouteClipToSourcesAndPlay(AudioClip clip, SFXCategory category, float pitchVariance, float volumeVariance)
+    {
+
+        float pitch = 1f + Random.Range(-pitchVariance, pitchVariance);
+        float volume = 1f + Random.Range(-volumeVariance, volumeVariance);
+
+        switch (category)
+        {
+            case SFXCategory.player:
+                {
+                    playerSfxSources[currentPlayerSource].pitch = pitch;
+                    playerSfxSources[currentPlayerSource].volume = volume;
+                    playerSfxSources[currentPlayerSource].PlayOneShot(clip);
+                    currentPlayerSource = (currentPlayerSource + 1) % playerSfxSources.Length;
+                }
+                break;
+            case SFXCategory.mob:
+                {
+                    mobSfxSources[currentMobSource].pitch = pitch;
+                    mobSfxSources[currentMobSource].volume = volume;
+                    mobSfxSources[currentMobSource].PlayOneShot(clip);
+                    currentMobSource = (currentMobSource + 1) % mobSfxSources.Length;
+                }
+                break;
+            case SFXCategory.ui:
+                {
+                    uiAudioSource.pitch = pitch;
+                    uiAudioSource.volume = volume;
+                    uiAudioSource.PlayOneShot(clip);
+                }
+
+                break;
+            default:
+                break;
+        }
+    }
+
+}
+
+public enum SFXCategory
+{
+    player,
+    mob,
+    ui
+}
---
+++ Assets/Scripts/Audio/AudioTesting.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class AudioTesting : MonoBehaviour
+{
+    public Dictionary<string, AudioClip> soundDictionary;
+    public Transform buttonParent;  
+    public GameObject soundButtonPrefab; 
+    public string testMultiSound;
+    public SFXCategory routingCategory;
+    public float testPitchVariance;
+    public float testVolVarince;
+
+    void Start()
+    {
+        soundDictionary = AudioManager.instance.audioDict;
+        foreach (KeyValuePair<string, AudioClip> clip in soundDictionary)
+        {
+            GameObject soundButton = Instantiate(soundButtonPrefab, buttonParent);
+            soundButton.GetComponentInChildren<TMP_Text>().SetText(clip.Key);
+            soundButton.GetComponent<Button>().onClick.AddListener(() => PlaySound(clip.Value));
+        }
+    }
+
+    void PlaySound(AudioClip sound)
+    {
+        AudioManager.instance.PlaySingleClip(sound.name, SFXCategory.player, testPitchVariance,testVolVarince );
+    }
+
+  public  void TestMultiSound()
+    {
+        AudioManager.instance.PlayShuffledSound(testMultiSound, SFXCategory.player, testPitchVariance, testVolVarince);
+    }
+}
