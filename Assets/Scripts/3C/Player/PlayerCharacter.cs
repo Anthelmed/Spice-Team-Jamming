30cb4a179340cc8192185ce4a31db354d9538fba
Erik Kubiak <erikkubiak@gmail.com>
2023-04-25 07:35:05 +0200

    Damped movement + setting up 3C done

---
+++ Assets/Scripts/3C/Player/PlayerCharacter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Runtime.Utilities;
+using UnityEngine;
+
+namespace _3C.Player
+{
+    
+    [RequireComponent(typeof(Rigidbody))]
+    public class PlayerCharacter : MonoBehaviour
+    {
+        [SerializeField] private float m_Speed;
+        [Min(0)]
+        [Tooltip("Defines how fast the character achieve player wanted speed, higher is the most reactive")]
+        [SerializeField] private float m_MovementDamping;
+        
+        
+        
+        [HideInInspector]
+        public Vector2 Movement;
+        private Rigidbody m_Rigidbody;
+
+        private Vector2 m_CurrentMovement;
+
+        private Vector3 CurrentWorldSpeed => (m_CurrentMovement * m_Speed).X0Y();  
+
+        private void Awake()
+        {
+            m_Rigidbody = GetComponent<Rigidbody>();
+        }
+
+        private void Update()
+        {
+            m_CurrentMovement = Vector2.LerpUnclamped(m_CurrentMovement, Movement, Time.deltaTime * m_MovementDamping);
+            m_Rigidbody.velocity = CurrentWorldSpeed;
+        }
+    }
+}
---
+++ Assets/Scripts/3C/Player/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.InputSystem;
+
+namespace _3C.Player
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] private PlayerCharacter m_PlayerCharacter;
+
+        private void Awake()
+        {
+            Assert.IsNotNull(m_PlayerCharacter, "Player Character should not be null");
+        }
+
+        public void OnMovementAsked(InputAction.CallbackContext _context)
+        {
+            m_PlayerCharacter.Movement = _context.ReadValue<Vector2>();
+        }
+
+        public void OnMeleeAttack(InputAction.CallbackContext _context)
+        {
+            
+        }
+
+        public void OnRangeAttack(InputAction.CallbackContext _context)
+        {
+            
+        }
+    }
+}
