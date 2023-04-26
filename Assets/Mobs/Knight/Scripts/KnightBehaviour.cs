3452153c42215a29646611e787e1a354162f5018
Jordi Caballol <jcaballol94@gmail.com>
2023-04-26 22:11:44 +0200

    Follow the target using the NavMesh

--- Assets/Mobs/Knight/Scripts/KnightBehaviour.cs
+++ Assets/Mobs/Knight/Scripts/KnightBehaviour.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,7 +15,7 @@ public class KnightBehaviour : MonoBehaviour
     [SerializeField] private Rigidbody m_rigidbody;
     [SerializeField] private Animator m_animator;
 
-    private readonly int SPEED = Animator.StringToHash("Speed");
+    private static readonly int SPEED = Animator.StringToHash("Speed");
 
     private enum State
     {
@@ -39,8 +40,80 @@ public class KnightBehaviour : MonoBehaviour
     private void Update()
     {
         m_animator.SetFloat(SPEED, m_rigidbody.velocity.magnitude);
+
+        // Update before just in case
+        UpdateTransition();
+
+        switch (m_state)
+        {
+            case State.Idle:
+                Idle_Update();
+                break;
+            case State.GoToTarget:
+                GoToTarget_Update();
+                break;
+        }
+
+        // Update after to pick changes during the update
+        UpdateTransition();
+    }
+
+    private void UpdateTransition()
+    {
+        if (m_state == m_nextState)
+            return;
+
+        switch (m_state)
+        {
+            case State.GoToTarget:
+                GoToTarget_Exit();
+                break;
+        }
+
+        switch (m_nextState)
+        {
+            case State.GoToTarget:
+                GoToTarget_Enter();
+                break;
+        }
+
+        m_state = m_nextState;
     }
 
     #region States
+    #region Idle
+    private void Idle_Update()
+    {
+        if (target)
+            m_nextState = State.GoToTarget;
+    }
+    #endregion
+
+    #region GoToTarget
+    private void GoToTarget_Enter()
+    {
+        // Set the destination to start moving
+        m_agent.SetDestination(target.position);
+    }
+
+    private void GoToTarget_Update()
+    {
+        if (!target)
+        {
+            m_nextState = State.Idle;
+            return;
+        }
+
+        if ((m_agent.destination - target.position).sqrMagnitude > (m_agent.radius * m_agent.radius))
+        {
+            m_agent.SetDestination(target.position);
+        }
+    }
+
+    private void GoToTarget_Exit()
+    {
+        m_agent.ResetPath();
+    }
+    #endregion
     #endregion
 }
