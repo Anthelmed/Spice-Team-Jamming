1090b5a8fe2c2b8ae98367387464525ab0a1a6e3
Jordi Caballol <jcaballol94@gmail.com>
2023-04-26 22:47:26 +0200

    Get into CombatIdle when we get close

--- Assets/Mobs/Knight/Scripts/KnightBehaviour.cs
+++ Assets/Mobs/Knight/Scripts/KnightBehaviour.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -8,10 +9,15 @@ using UnityEngine.AI;
 [RequireComponent(typeof(Animator))]
 public class KnightBehaviour : MonoBehaviour
 {
+    [Header("Public stuff")]
     public Transform target;
 
+    [Header("Referencies")]
     [SerializeField] private NavMeshAgent m_agent;
     [SerializeField] private Animator m_animator;
+
+    [Header("Parameters")]
+    [SerializeField] private Vector2 m_attackRange = Vector2.up;
 
     private static readonly int SPEED = Animator.StringToHash("Speed");
 
@@ -20,11 +26,13 @@ public class KnightBehaviour : MonoBehaviour
     private enum State
     {
         Idle,
-        GoToTarget
+        GoToTarget,
+        CombatIdle
     }
 
     private State m_state = State.Idle;
     private State m_nextState = State.Idle;
+    private bool m_lookAtTarget = false;
 
     private void OnValidate()
     {
@@ -54,14 +62,27 @@ public class KnightBehaviour : MonoBehaviour
             case State.GoToTarget:
                 GoToTarget_Update();
                 break;
+            case State.CombatIdle:
+                CombatIdle_Update();
+                break;
         }
 
         // Update after to pick changes during the update
         UpdateTransition();
 
+        // Update the animations
         var movement = transform.position - m_lastPosition;
         m_lastPosition = transform.position;
         m_animator.SetFloat(SPEED, movement.magnitude / Time.deltaTime);
+
+        // Update the rotation
+        if (m_lookAtTarget)
+        {
+            transform.rotation = Quaternion.RotateTowards(
+                transform.rotation,
+                Quaternion.LookRotation(target.position - transform.position),
+                m_agent.angularSpeed * Time.deltaTime);
+        }
     }
 
     private void UpdateTransition()
@@ -74,6 +95,9 @@ public class KnightBehaviour : MonoBehaviour
             case State.GoToTarget:
                 GoToTarget_Exit();
                 break;
+            case State.CombatIdle:
+                CombatIdle_Exit();
+                break;
         }
 
         switch (m_nextState)
@@ -81,9 +105,20 @@ public class KnightBehaviour : MonoBehaviour
             case State.GoToTarget:
                 GoToTarget_Enter();
                 break;
+            case State.CombatIdle:
+                CombatIdle_Enter();
+                break;
         }
 
         m_state = m_nextState;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, m_attackRange.x);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, m_attackRange.y);
     }
 
     #region States
@@ -100,6 +135,7 @@ public class KnightBehaviour : MonoBehaviour
     {
         // Set the destination to start moving
         m_agent.SetDestination(target.position);
+        m_agent.updateRotation = true;
     }
 
     private void GoToTarget_Update()
@@ -110,6 +146,14 @@ public class KnightBehaviour : MonoBehaviour
             return;
         }
 
+        // Go to combat if we are close enough
+        if ((target.position - transform.position).sqrMagnitude < (m_attackRange.y * m_attackRange.y))
+        {
+            m_nextState = State.CombatIdle;
+            return;
+        }
+
+        // Update the navigation path if the current one is too outdated
         if ((m_agent.destination - target.position).sqrMagnitude > (m_agent.radius * m_agent.radius))
         {
             m_agent.SetDestination(target.position);
@@ -119,6 +163,34 @@ public class KnightBehaviour : MonoBehaviour
     private void GoToTarget_Exit()
     {
         m_agent.ResetPath();
+        m_agent.updateRotation = false;
+    }
+    #endregion
+
+    #region CombatIdle
+    private void CombatIdle_Enter()
+    {
+        m_lookAtTarget = true;
+    }
+
+    private void CombatIdle_Update()
+    {
+        if (!target)
+        {
+            m_nextState = State.Idle;
+            return;
+        }
+
+        if ((target.position - transform.position).sqrMagnitude > (m_attackRange.y * m_attackRange.y))
+        {
+            m_nextState = State.GoToTarget;
+            return;
+        }
+    }
+
+    private void CombatIdle_Exit()
+    {
+        m_lookAtTarget = false;
     }
     #endregion
     #endregion
