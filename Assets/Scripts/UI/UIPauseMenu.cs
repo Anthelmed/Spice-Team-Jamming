58bee07748026902dc3b42ca2bc45b1ed198fc12
ifs-anthelme <anthelme.dumont@gmail.com>
2023-04-27 20:19:34 +0200

    refactor stylen add missing ui documents, import UI utils, create first class

---
+++ Assets/Scripts/UI/UIPauseMenu.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UIToolkitAutoReferences;
+using UnityEngine;
+using UnityEngine.UIElements.Experimental;
+
+namespace DefaultNamespace.UI
+{
+	public class UIPauseMenu : MonoBehaviour
+	{
+		[SerializeField] private GameManager gameManager;
+		[SerializeField] private PauseMenuDocumentAutoReferences elementsReferences;
+		
+		private void OnEnable()
+		{
+			//Register to GameManager events
+		}
+
+		private void OnDisable()
+		{
+			//Unregister from GameManager events
+		}
+
+		private void Show()
+		{
+			UIAnimationsUtils.FadeIn(elementsReferences.PauseMenu, 300, Easing.InOutQuad);
+		}
+
+		private void Hide()
+		{
+			UIAnimationsUtils.FadeIn(elementsReferences.PauseMenu, 100, Easing.InOutQuad);
+		}
+	}
+}
