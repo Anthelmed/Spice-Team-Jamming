5beb5bc46e03ae4768bf26b5199e34ae75c2d16f
Erik Kubiak <erikkubiak@gmail.com>
2023-04-29 08:12:52 +0200

    Range attack working + ordering of the character prefab + project

--- Assets/Scripts/3C/Player/Attacks/PlayerRangeAttack.cs
+++ Assets/Scripts/3C/Player/Attacks/PlayerRangeAttack.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using _3C.Player.Weapons;
+using DefaultNamespace.HealthSystem.Damager;
 using UnityEngine;
 
 namespace _3C.Player
@@ -6,21 +9,57 @@ namespace _3C.Player
     [Serializable]
     public class PlayerRangeAttack : PlayerStateBehavior
     {
+        [Header("Settings")]
+        [SerializeField] private float m_AttackDuration;
+        [SerializeField] private AnimationCurve m_AttackAnimationCurve;
+        [SerializeField] private int m_BaseDamage;
+
+        [Header("Scene components")]
+        [SerializeField] private ColliderDamager m_Damager;
+        [Tooltip("This one is used to hold the weapon and the vfx to unparent them")]
+        [SerializeField] private Transform m_AttackHolder;
+        
+        [SerializeField] private AWeaponMovement m_RangeWeaponMovement;
+        [SerializeField] private ParticleSystem[] m_VFXToPlay;
+        
+        [Tooltip("Planed for world UI, use to enable the thing you need during aiming, will be disabled at the end of aiming")]
+        [SerializeField] private GameObject m_AimingWorldUI;
+        
+        
+        
         private Transform m_Transform;
 
         protected override void Init(IStateHandler _stateHandler)
         {
             m_Transform = _stateHandler.gameObject.transform;
+            if (m_AimingWorldUI != null)
+            {
+                m_AimingWorldUI.SetActive(false);
+            }
         }
 
         public override void StartState()
         {
             m_StateHandler.OnAimingStateChanged(true);
+            if (m_AimingWorldUI != null)
+            {
+                m_AimingWorldUI.SetActive(true);
+            }
         }
 
         public override void StopState()
         {
             m_StateHandler.OnAimingStateChanged(false);
+            StateCleaning();
+            PostAttackCleaning();
+        }
+
+        private void StateCleaning()
+        {
+            if (m_AimingWorldUI != null)
+            {
+                m_AimingWorldUI.SetActive(false);
+            }
         }
 
         public override void OnInput(InputType inputType)
@@ -28,7 +67,39 @@ namespace _3C.Player
             if (inputType == InputType.AimCanceled)
             {
                 m_StateHandler.OnStateEnded();
+                TriggerAttack();
                 //TODO: TriggerAttackAfterDelay
+            }
+        }
+
+        private void TriggerAttack()
+        {
+            StateCleaning();
+            m_Damager.Damage = m_BaseDamage;
+            m_AttackHolder.SetParent(null, true);
+            m_RangeWeaponMovement.TriggerWeaponMovement(m_AttackDuration, m_AttackAnimationCurve);
+            foreach (var particleSystem in m_VFXToPlay)
+            {
+                particleSystem.Play();
+            }
+
+            m_StateHandler.StartCoroutine(c_OnAttackAnimationEnded());
+        }
+
+        private IEnumerator c_OnAttackAnimationEnded()
+        {
+            yield return new WaitForSeconds(m_AttackDuration);
+            PostAttackCleaning();
+        }
+
+        private void PostAttackCleaning()
+        {
+            m_RangeWeaponMovement.transform.localPosition = Vector3.zero;
+            m_RangeWeaponMovement.transform.localRotation = Quaternion.identity;
+            m_AttackHolder.SetParent(m_Transform, true);
+            foreach (var particleSystem in m_VFXToPlay)
+            {
+                particleSystem.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
             }
         }
     }
---
+++ Assets/Scripts/3C/Player/Attacks/Weapons/ForwardMovementWeapon.cs
@@ -0,0 +1,16 @@
+﻿using _3C.Player.Weapons;
+using DG.Tweening;
+using UnityEngine;
+
+public class ForwardWeaponMovement : AWeaponMovement
+{
+    [SerializeField] private float m_Speed;
+
+    protected override Tween TriggerTween(float _duration)
+    {
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.identity;
+
+        return transform.DOMove(transform.position + transform.forward * m_Speed * _duration, _duration);
+    }
+}
