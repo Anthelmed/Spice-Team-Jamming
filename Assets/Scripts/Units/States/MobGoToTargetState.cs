da45ca636d12ab4dba2957a817385b865c5006b1
Jordi Caballol <jcaballol94@gmail.com>
2023-04-29 23:35:11 +0200

    Outline of the GoToTarget

--- Assets/Scripts/Units/States/MobGoToTargetState.cs
+++ Assets/Scripts/Units/States/MobGoToTargetState.cs
@@ -8,14 +8,25 @@ namespace Units
     {
         public void Enter(Mob.Data data)
         {
+            // TODO: Set destination in locomotion
+            // TODO: Locomotion look in direction of movement
+
+            if (data.perception) data.perception.QueryTargetEnabled = true;
         }
 
         public void Exit(Mob.Data data)
         {
+            // TODO: Locomotion clear destination
+            // TODO: Locomotion stop rotation
+
+            if (data.perception) data.perception.QueryTargetEnabled = false;
         }
 
         public void Tick(Mob.Data data)
         {
+            // TODO: If leader too far, Regroup
+            // TODO: If missing target, Idle
+            // TODO: If close enough, fight
         }
     }
 }
