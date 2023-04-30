010136f7803e38afa48e60a8c2f2e22423b2a781
Erik Kubiak <erikkubiak@gmail.com>
2023-04-30 18:34:04 +0200

    Basic tutorial stuff

---
+++ Assets/Scripts/GameStaticEvents.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using DefaultNamespace.Tutorial;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class GameStaticEvents
+    {
+        public static Action<TutorialEvent> OnTutorialAsked;
+        public static Action<string, string, Sprite> OnDisplayPopUp;
+    }
+}
