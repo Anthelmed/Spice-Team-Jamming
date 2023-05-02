f73d203e45aa545f33c8ca0626f121f88ac6c60a
Jordi Caballol <jcaballol94@gmail.com>
2023-05-02 17:31:59 +0200

    Recover missing random scripts

---
+++ Assets/Mobs/Scripts/RandomOrientation.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class RandomOrientation : MonoBehaviour
+{
+    void Start()
+    {
+        transform.localRotation = Quaternion.AngleAxis(Random.Range(0, 360f), Vector3.up);
+    }
+}
